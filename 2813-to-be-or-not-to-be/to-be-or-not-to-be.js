c38431ca86fe302d5ada52b2bdb93a2c779c7365
Abhijeet Kasera <146567228+ABHIJEET-0001@users.noreply.github.com>
2026-04-01 06:43:09 +0530

    Time: 40 ms (77.73%) | Memory: 52.5 MB (97.01%) - LeetSync

---
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -0,0 +1,18 @@
+var expect = function(val) {
+    return {
+        toBe: function(value) {
+            if (val === value) {
+                return true;
+            } else {
+                throw new Error("Not Equal");
+            }
+        },
+        notToBe: function(value) {
+            if (val !== value) {
+                return true;
+            } else {
+                throw new Error("Equal");
+            }
+        }
+    };
+};
